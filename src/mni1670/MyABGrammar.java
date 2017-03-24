927ab1d3cd9cdc297145d1e10869d31adffd18eb
Mathias Nigsch <mathias.nigsch@gmail.com>
2017-03-24 09:24:04 +0100

    Fixed parser

--- src/mni1670/MyABGrammar.java
+++ src/mni1670/MyABGrammar.java
@@ -8,13 +8,19 @@ public class MyABGrammar implements MyABGrammarConstants {
     try
     {
       MyABGrammar parser = new MyABGrammar(new FileInputStream("text.txt"));
-
       while (true)
       {
         System.out.println("Reading ...");
         try
         {
-          parser.s();
+          switch(parser.read_one_line()) {
+                                case 0:
+                                break;
+
+                                case 1:
+                                System.out.println("Finished");
+                                break;
+          }
         }
         catch (Exception e)
         {
@@ -40,6 +46,25 @@ public class MyABGrammar implements MyABGrammarConstants {
 // VN = { S,A, B }
 // S - > a | aB
 // B - > b | bB
+  final public int read_one_line() throws ParseException {
+    switch ((jj_ntk==-1)?jj_ntk():jj_ntk) {
+    case A:
+      s();
+      jj_consume_token(0);
+    {if (true) return 0;}
+      break;
+    case 0:
+      jj_consume_token(0);
+    {if (true) return 1;}
+      break;
+    default:
+      jj_la1[0] = jj_gen;
+      jj_consume_token(-1);
+      throw new ParseException();
+    }
+    throw new Error("Missing return statement in function");
+  }
+
   final public void s() throws ParseException {
     jj_consume_token(A);
     switch ((jj_ntk==-1)?jj_ntk():jj_ntk) {
@@ -47,10 +72,9 @@ public class MyABGrammar implements MyABGrammarConstants {
       b();
       break;
     default:
-      jj_la1[0] = jj_gen;
+      jj_la1[1] = jj_gen;
       ;
     }
-    jj_consume_token(0);
   }
 
   final public void b() throws ParseException {
@@ -62,7 +86,7 @@ public class MyABGrammar implements MyABGrammarConstants {
         ;
         break;
       default:
-        jj_la1[1] = jj_gen;
+        jj_la1[2] = jj_gen;
         break label_1;
       }
       jj_consume_token(B);
@@ -78,13 +102,13 @@ public class MyABGrammar implements MyABGrammarConstants {
   public Token jj_nt;
   private int jj_ntk;
   private int jj_gen;
-  final private int[] jj_la1 = new int[2];
+  final private int[] jj_la1 = new int[3];
   static private int[] jj_la1_0;
   static {
       jj_la1_init_0();
    }
    private static void jj_la1_init_0() {
-      jj_la1_0 = new int[] {0x100,0x100,};
+      jj_la1_0 = new int[] {0x81,0x100,0x100,};
    }
 
   /** Constructor with InputStream. */
@@ -98,7 +122,7 @@ public class MyABGrammar implements MyABGrammarConstants {
     token = new Token();
     jj_ntk = -1;
     jj_gen = 0;
-    for (int i = 0; i < 2; i++) jj_la1[i] = -1;
+    for (int i = 0; i < 3; i++) jj_la1[i] = -1;
   }
 
   /** Reinitialise. */
@@ -112,7 +136,7 @@ public class MyABGrammar implements MyABGrammarConstants {
     token = new Token();
     jj_ntk = -1;
     jj_gen = 0;
-    for (int i = 0; i < 2; i++) jj_la1[i] = -1;
+    for (int i = 0; i < 3; i++) jj_la1[i] = -1;
   }
 
   /** Constructor. */
@@ -122,7 +146,7 @@ public class MyABGrammar implements MyABGrammarConstants {
     token = new Token();
     jj_ntk = -1;
     jj_gen = 0;
-    for (int i = 0; i < 2; i++) jj_la1[i] = -1;
+    for (int i = 0; i < 3; i++) jj_la1[i] = -1;
   }
 
   /** Reinitialise. */
@@ -132,7 +156,7 @@ public class MyABGrammar implements MyABGrammarConstants {
     token = new Token();
     jj_ntk = -1;
     jj_gen = 0;
-    for (int i = 0; i < 2; i++) jj_la1[i] = -1;
+    for (int i = 0; i < 3; i++) jj_la1[i] = -1;
   }
 
   /** Constructor with generated Token Manager. */
@@ -141,7 +165,7 @@ public class MyABGrammar implements MyABGrammarConstants {
     token = new Token();
     jj_ntk = -1;
     jj_gen = 0;
-    for (int i = 0; i < 2; i++) jj_la1[i] = -1;
+    for (int i = 0; i < 3; i++) jj_la1[i] = -1;
   }
 
   /** Reinitialise. */
@@ -150,7 +174,7 @@ public class MyABGrammar implements MyABGrammarConstants {
     token = new Token();
     jj_ntk = -1;
     jj_gen = 0;
-    for (int i = 0; i < 2; i++) jj_la1[i] = -1;
+    for (int i = 0; i < 3; i++) jj_la1[i] = -1;
   }
 
   private Token jj_consume_token(int kind) throws ParseException {
@@ -206,7 +230,7 @@ public class MyABGrammar implements MyABGrammarConstants {
       la1tokens[jj_kind] = true;
       jj_kind = -1;
     }
-    for (int i = 0; i < 2; i++) {
+    for (int i = 0; i < 3; i++) {
       if (jj_la1[i] == jj_gen) {
         for (int j = 0; j < 32; j++) {
           if ((jj_la1_0[i] & (1<<j)) != 0) {
